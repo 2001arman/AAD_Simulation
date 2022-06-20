04653bb01879d8f2b7f61112ee596635fd7f577f
Arman <2001armanmaulana@gmail.com>
2022-06-20 12:59:12 +0800

    progress

--- To-DoListApp/app/src/main/java/com/dicoding/todoapp/data/TaskRepository.kt
+++ To-DoListApp/app/src/main/java/com/dicoding/todoapp/data/TaskRepository.kt
@@ -2,7 +2,10 @@ package com.dicoding.todoapp.data
 
 import android.content.Context
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import androidx.paging.LivePagedListBuilder
 import androidx.paging.PagedList
+import com.dicoding.todoapp.utils.FilterUtils
 import com.dicoding.todoapp.utils.TasksFilterType
 
 class TaskRepository(private val tasksDao: TaskDao) {
@@ -29,7 +32,18 @@ class TaskRepository(private val tasksDao: TaskDao) {
     //TODO 4 : Use FilterUtils.getFilteredQuery to create filterable query
     //TODO 5 : Build PagedList with configuration
     fun getTasks(filter: TasksFilterType): LiveData<PagedList<Task>> {
-        throw NotImplementedError("Not yet implemented")
+        val query = FilterUtils.getFilteredQuery(filter)
+        val config = PagedList.Config.Builder().apply {
+            setEnablePlaceholders(PLACEHOLDERS)
+            setPageSize(PAGE_SIZE)
+            setInitialLoadSizeHint(PAGE_SIZE)
+        }.build()
+        val mediatorLiveData = MediatorLiveData<PagedList<Task>>()
+        val liveData = LivePagedListBuilder(tasksDao.getTasks(query), config).build()
+        mediatorLiveData.addSource(liveData) {
+            mediatorLiveData.value = it
+        }
+        return mediatorLiveData
     }
 
     fun getTaskById(taskId: Int): LiveData<Task> {
